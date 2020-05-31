e76df5c0a6bc477d730807bfb6b1f3059761c0fd
Rachel Isaac-Wilson <rachel.isaac.wilson@gmail.com>
2020-05-31 23:43:23 +0100

    wall 1

---
+++ Wall.js
@@ -0,0 +1,41 @@
+
+const theWallList = fetch("https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall");
+
+theWallList.then(response => response.json()).then(data => addElements(data));
+
+
+function addElements(list){
+  list.forEach (function (element) {
+    let newLi = document.createElement("li")
+    let newContent = document.createTextNode(element)
+    newLi.appendChild(newContent)
+    let currentUl = document.getElementById("list")
+    document.body.insertBefore(newLi, currentUl)
+  })
+};
+
+function addComment(comment) {
+  let li = document.createElement("li");
+  document.getElementById("list").appendChild(li);
+
+  li.innerHTML += comment.value;
+  return false;
+  }
+  
+  
+fetch("https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall", {
+  method: "POST",
+  body: JSON.stringify({message: ""})
+}).then(function (response) {
+  if (response.status == 200) {
+    console.log("Ok!")
+  } else {console.log("Oh no!")}
+})
+
+fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(data => console.log(data))
+
+if (localStorage.getItem("test") == "key") {
+  console.log("It's there!")
+} else {
+  localStorage.setItem("test", "key")
+}
